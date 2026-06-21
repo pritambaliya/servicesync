b5b818375c3cb8267b967ab7311b14b58c254632
Prit Ambaliya <pritambaliya123@gmail.com>
2026-06-21 21:58:26 +0530

    Change socket CORS

--- server/config/socket.js
+++ server/config/socket.js
@@ -6,7 +6,7 @@ export const initSocket = (server) => {
 
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://servicesync-enrn.onrender.com",
       credentials: true,
     },
   });
