adf0b2aa2459872fd3991d6dbbb00c3abc84c751
pritambaliya <pritambaliya123@gmail.com>
2026-06-22 16:31:37 +0530

    add jsonwebtoken dependency

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,42 +1,48 @@
 import express from "express";
 import passport from "passport";
 import { logoutUser, sendForgotOtp, resetPassword } from "../controllers/auth.controller.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/login", (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
-    
+
     if (err) {
       return res.status(500).json({
-        success: false,
-        message: "Server error"
+        success:false,
+        message:"Server error"
       });
     }
 
+
     if (!user) {
       return res.status(400).json({
-        success: false,
+        success:false,
         message: info?.message || "Login failed"
       });
     }
 
-    req.login(user, (err) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          message: "Login session error"
-        });
+    const token = jwt.sign(
+      {
+        id:user._id
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn:"7d"
       }
+    );
 
-      return res.json({
-        success: true,
-        message: "Login successful",
-        user
-      });
+    return res.json({
+      success:true,
+      message:"Login successful",
+      token,
+      user
     });
 
-  })(req, res, next);
+
+  })(req,res,next);
+
 });
 
 router.get("/logout", logoutUser);
