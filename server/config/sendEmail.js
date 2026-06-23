dcc6744c5ad83b3247105c54c4c017993ee96922
pritambaliya <pritambaliya123@gmail.com>
2026-06-23 21:31:48 +0530

    fixed email

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -1,25 +1,16 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-dns.setDefaultResultOrder("ipv4first");
+import dotenv from "dotenv";
+dotenv.config();
 
 const sendEmail = async (to, subject, html) => {
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      requireTLS: true,
-      family: 4,
+      service:"gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
       }
     });
-
-    await transporter.verify();
-    console.log("SMTP working");
-    console.log(process.env.EMAIL_USER)
 
     await transporter.sendMail({
       from: `"ServiceSync Support" <${process.env.EMAIL_USER}>`,
