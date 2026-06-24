01ed36c5e75444d718b803ba03c73cfd01ff8ff8
pritambaliya <pritambaliya123@gmail.com>
2026-06-24 07:02:08 +0530

    fixed resend email1

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -1,26 +1,34 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
+const transporter = nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    auth: {
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS
+    }
+});
 
 const sendEmail = async (to, subject, html) => {
 
     try {
-        console.log("Sending email through Resend...");
-        const result = await resend.emails.send({
-            from: "ServiceSync <onboarding@resend.dev>",
+        console.log("Sending email through Brevo...");
+        const result = await transporter.sendMail({
+
+            from: `ServiceSync <${process.env.EMAIL_USER}>`,
             to,
             subject,
             html
         });
-        console.log("✅ Email sent:", result);
+        console.log("✅ Email sent:", result.messageId);
+
 
     } catch (error) {
-        console.log("❌ Resend Error:", error.message);
+        console.log("❌ Brevo Error:", error.message);
         throw error;
+
     }
 
 };
